6e2adb8db90ec9cc55b65a90c25b67b080b85474
ashtreehugs <agrev8@gmail.com>
2020-03-08 12:44:58 -0400

    Remove location: @post in PostsController

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -25,7 +25,7 @@ class PostsController < OpenReadController
     @post = current_user.posts.build(post_params)
 
     if @post.save
-      render json: @post, status: :created, location: @post
+      render json: @post, status: :created
     else
       render json: @post.errors, status: :unprocessable_entity
     end
